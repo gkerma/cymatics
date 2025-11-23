462e451bd6afcb370153af9140c142048e332f3e
CyberMind <devel@cybermind.fr>
2025-11-23 06:46:38 +0100

    Update stepseq.py

--- sequencer/stepseq.py
+++ sequencer/stepseq.py
@@ -1,1 +1,11 @@
 # Step sequencer placeholder
+import numpy as np
+
+def step_sequence(pattern, freq, synth_fn, sr, step_len=0.25):
+    audio = []
+    for step in pattern:
+        if step:
+            audio.append(synth_fn(freq, sr, step_len))
+        else:
+            audio.append(np.zeros(int(sr*step_len)))
+    return np.concatenate(audio)
